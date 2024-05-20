7acc5ddb0cfcca612560dad71e19ee229b1b389d
Sérgio Adriano <sergioadrianomc@gmail.com>
2024-05-20 14:48:01 -0300

    correção efetuada

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+    const props = defineProps(['tarefasPendentes']);
+
+</script>
+
+<template>
+        <!--MINHAS TAREFAS-->
+        <header class="p-5 mb-4 mt-4 bg-minhasTarefas rounded-3">
+            <h1>Minhas Tarefas</h1>
+            <p>
+                Você possui {{ props.tarefasPendentes }} tarefas.
+            </p>
+        </header>
+</template>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,34 @@
+<script setup>
+    const props = defineProps(['cadastraTarefa', 'tarefaTemp', 'trocarFiltro', 'editaTarefaTemp']);
+</script>
+
+<template>
+<!--FORMULÁRIO DE TAREFAS-->
+    <form @submit.prevent="props.cadastraTarefa">
+        <div class="row">
+            <!--Coluna 1-->
+            <div class="col">
+                <input
+                    :value="props.tarefaTemp"
+                    @change="props.editaTarefaTemp"
+                    required
+                    type="text"
+                    placeholder="Digite a descrição da tarefa:"
+                    class="form-control"
+                />
+            </div>
+            <!--Coluna 2-->
+            <div class="col-md-2">
+                <button type="submit" class="btn btn-primary">Cadastrar</button>
+            </div>
+            <!--Coluna 3-->
+            <div class="col-md-2">
+                <select @change="props.trocarFiltro">
+                    <option value="todas">Todas as Tarefas</option>
+                    <option value="pendentes">Pendentes</option>
+                    <option value="finalizada">Finalizadas</option>
+                </select>
+            </div>
+        </div>
+    </form>
+</template>
